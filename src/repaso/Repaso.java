e5396e22df60589cf7ccbe003a63266e36f5f933
unknown <marisol91olmos@gmail.com>
2023-08-22 21:29:02 -0300

    TIPOS DE DATOS PRIMITIVOS

--- src/repaso/Repaso.java
+++ src/repaso/Repaso.java
@@ -15,7 +15,37 @@ public class Repaso {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //VARIABLES
+        /*
+        -Representa un espacio en la memoria en el cual podemos guardar 
+        distintos tipos de datos
+        -Se le pueden asignar nombres 
+        -Asignar tipos de datos 
+       
+        */
+        //int nicoEdad = 38;
+        //String nicoNombre = "Nicolas";
+        
+        
+        //System.out.printIn("Me llamo: " + nicoNombre);
+        //System.out.println("Y tengo: " +nicoEdad);
+        
+        //TIPOS DE DATOS PRIMITIVOS
+        /*
+        -int: almacena numeros enteros
+        -boolean: es un valor lógico que puede ser true o false
+        -float: almacena números reales con una precisión de 
+        7 dígitos decimales.
+        -short: un número entero de 16 bits con signo
+        
+       
+        */       
+        //ejemplo booleano:
+        int numero =5;
+        boolean esMayorQueCero = numero> 0;
+        
+        System.out.print("El numero es mayor que cero: "+esMayorQueCero);
+        
     }
     
 }
